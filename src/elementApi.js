67d12dd9404f0ed65de996f17b5d59813cc38b8e
Sharoon Arif <sharoon.arif@asos.com>
2018-10-26 11:07:50 +0100

    Refactored value accessor method

--- src/elementApi.js
+++ src/elementApi.js
@@ -1,3 +1,5 @@
+const getFloatFromTarget = target => parseFloat(target.value);
+
 const initHandlers = () => {
 	document.getElementById('reset-btn').addEventListener('click', reset);
 	document.getElementById('stop-btn').addEventListener('click', stop);
@@ -5,26 +7,22 @@ const initHandlers = () => {
 	document.getElementById('clear-btn').addEventListener('click', clear);
 	document.getElementById('random-btn').addEventListener('click', randomize);
 	document.getElementById('opacity').addEventListener('change', updateDrawParameter.bind(this, 'opacity', target => parseFloat(target.value)));
-	document.getElementById('outer-speed').addEventListener('change', updateDrawParameter.bind(this, 'outerSpeed', target => parseFloat(target.value)));
-	document.getElementById('inner-speed').addEventListener('change', updateDrawParameter.bind(this, 'innerSpeed', target => parseFloat(target.value)));
-	document.getElementById('overall-speed').addEventListener('change', updateDrawParameter.bind(this, 'overallSpeed', target => parseFloat(target.value)));
-	document.getElementById('circle-size').addEventListener('change', updateDrawParameter.bind(this, 'innerCircleRadius', target => parseFloat(target.value)));
-	document.getElementById('pencil-offset').addEventListener('change', updateDrawParameter.bind(this, 'pencilOffset', target => parseFloat(target.value)));
-	document.getElementById('pencil-size').addEventListener('change', updateDrawParameter.bind(this, 'pencilSize', target => parseFloat(target.value)));
+	document.getElementById('outer-speed').addEventListener('change', updateDrawParameter.bind(this, 'outerSpeed', getFloatFromTarget));
+	document.getElementById('inner-speed').addEventListener('change', updateDrawParameter.bind(this, 'innerSpeed', getFloatFromTarget));
+	document.getElementById('overall-speed').addEventListener('change', updateDrawParameter.bind(this, 'overallSpeed', getFloatFromTarget));
+	document.getElementById('circle-size').addEventListener('change', updateDrawParameter.bind(this, 'innerCircleRadius', getFloatFromTarget));
+	document.getElementById('pencil-offset').addEventListener('change', updateDrawParameter.bind(this, 'pencilOffset', getFloatFromTarget));
+	document.getElementById('pencil-size').addEventListener('change', updateDrawParameter.bind(this, 'pencilSize', getFloatFromTarget));
 	document.getElementById('hide-stencils').addEventListener('change', updateDrawParameter.bind(this, 'hideStencils', target => target.checked));
 
 	const colorPicker = document.getElementById('color-picker');
+	const getColorFromDataColor = (target) => target.attributes['data-color'].value;
 	for (const child of colorPicker.children) {
-		child.addEventListener('click', updateDrawParameter.bind(this, 'strokeColor', (target) => target.attributes['data-color'].value));
+		child.addEventListener('click', updateDrawParameter.bind(this, 'strokeColor', getColorFromDataColor));
 	}
 };
 
-const getContext = () => {
-	const canvas = document.getElementById('changs-canvas');
-	return canvas.getContext('2d');
-};
-
 const getContextById = id => {
-	const canvas = document.getElementById(id);	
+	const canvas = document.getElementById(id);
 	return canvas.getContext('2d');
 };
